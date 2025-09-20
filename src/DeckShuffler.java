c18fa23937aefb9d80ccac38cb685909bb3b8c18
Eren <ernkrc005@gmail.com>
2025-09-20 12:58:51 +0300

    shuffler changes 2

--- src/DeckShuffler.java
+++ src/DeckShuffler.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DeckShuffler {
     public static void suffle_deck(ArrayList<String> deck){
-
+        Collections.shuffle(deck);
     }
 }
