c7f13b94b1d1806097c7435f19ea06df466f9cc0
Eren Karcı <72691957+FredJones35@users.noreply.github.com>
2025-09-20 19:56:43 +0300

    Delete JavaApplication3 directory

--- JavaApplication3/src/DeckShuffler.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class DeckShuffler {
-    public static void suffle_deck(ArrayList<String> deck){
-        Collections.shuffle(deck);
-    }
-}
